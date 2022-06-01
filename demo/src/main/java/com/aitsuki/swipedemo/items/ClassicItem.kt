fa82ff6722f6bd97296d7e17e825f4c2580b2c4a
aitsuki <aitsukigo@gmail.com>
2022-06-01 17:37:59 +0800

    Add demo to show how to open the menu with long press.

--- demo/src/main/java/com/aitsuki/swipedemo/items/ClassicItem.kt
+++ demo/src/main/java/com/aitsuki/swipedemo/items/ClassicItem.kt
@@ -1,8 +1,10 @@
 package com.aitsuki.swipedemo.items
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.aitsuki.swipe.SwipeLayout
 import com.aitsuki.swipedemo.BaseItem
 import com.aitsuki.swipedemo.databinding.ItemClassicBinding
 
@@ -16,6 +18,20 @@ class ClassicItem : BaseItem<ItemClassicBinding>() {
         binding.content.text = "Classic $position"
         binding.content.setOnClickListener {
             Toast.makeText(it.context, "Classic $position", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.root.addListener(object : SwipeLayout.Listener {
+            override fun onSwipeStateChanged(menuView: View, newState: Int) {
+                if (newState == SwipeLayout.STATE_IDLE) {
+                    binding.root.swipeFlags = SwipeLayout.LEFT or SwipeLayout.RIGHT
+                }
+            }
+        })
+
+        binding.content.setOnLongClickListener {
+            binding.root.swipeFlags = 0
+            binding.root.openRightMenu(true)
+            true
         }
         binding.leftMenu.setOnClickListener {
             Toast.makeText(it.context, "LEFT $position", Toast.LENGTH_SHORT).show()
